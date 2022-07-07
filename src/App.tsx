38cb76cb735d60b6e8740edcb995d3e6a62f86c8
KSoft-Solution <kanhasahu955902@gmail.com>
2022-07-08 00:50:28 +0530

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import axios from 'axios'
 
 const App:React.FC<any> = () => {
+  useEffect(()=>{
+    axios.get('https://restful-api-gamma.vercel.app/').then((res)=>{
+      console.log(res)
+    }).catch((error:Error)=>{
+      console.log(error?.message)
+    })
+  },[])
   return (
     <div>
       hello world
